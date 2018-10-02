6ae1ad9c6793367e30276be198098cd549dd0e1d
iino <iino@soba-project.com>
2018-10-02 17:51:37 +0900

    combine reducers

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import { createStore } from 'redux'
-import CounterReducer from './reducers/counter'
+import reducers from './reducers/index'
 import * as CounterAction from './actions/counter'
 console.log('---> redux start')
 
 // --------------------------------------------------
 //   store
-const store = createStore(CounterReducer)
+const store = createStore(reducers)
 store.subscribe(() => console.log(store.getState()))
 store.dispatch(CounterAction.increment(1))
 store.dispatch(CounterAction.decrement(1))
