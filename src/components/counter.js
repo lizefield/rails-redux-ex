c17dc15f68dcc64c1cd374557bf9e3805be9181d
Koji Iino <lizefield@gmail.com>
2018-10-03 02:15:35 +0900

    change presentation

--- src/components/counter.js
+++ src/components/counter.js
@@ -12,9 +12,14 @@ export default class Counter extends React.Component {
       <div>
         <h1>Counter</h1>
         <h2>{counter.count}</h2>
+        {this.checkCount(counter.count)}
         <button onClick={(e) => increment(e)}>INCREMENT</button>
         <button onClick={(e) => decrement(e)}>DECREMENT</button>
       </div>
     )
+  }
+
+  checkCount(count) {
+    return (count > 10) ? <p style={{color: 'red'}}>Over 10</p> : <p style={{color: 'blue'}}>Under 10</p>
   }
 }
