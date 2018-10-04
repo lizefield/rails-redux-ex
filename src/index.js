3e23ed07718f8a4ce77f8256f179cc5a10b9bd0e
iino <iino@soba-project.com>
2018-10-04 12:11:32 +0900

    add router

--- src/index.js
+++ src/index.js
@@ -3,17 +3,30 @@ import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import thunkMiddleware from 'redux-thunk'
+import { createBrowserHistory } from 'history'
+import { Route, Switch } from 'react-router'
+import { RouterState, connectRouter, routerMiddleware, ConnectedRouter } from 'connected-react-router'
 import reducers from './reducers/index'
-import Index from './components/index'
+import Login from './containers/account'
+
+const history = createBrowserHistory()
 
 const store = createStore(
-    reducers,
-    applyMiddleware(thunkMiddleware)
+    connectRouter(history)(reducers),
+    { router: RouterState },
+    applyMiddleware(thunkMiddleware, routerMiddleware(history))
   )
 
 render(
   <Provider store={store}>
-    <Index />
+    <ConnectedRouter history={history}>
+      <div>
+        <Switch>
+          <Route exact path='/' component={Login} />
+          <Route render={() => (<div>No Content</div>)} />
+        </Switch>
+      </div>
+    </ConnectedRouter>
   </Provider>,
   document.getElementById('redux-app')
 )
